8d0dc243de549fbcba1bc1af931b8922beb55c08
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2015-12-14 17:44:52 +0100

    Few changes to the scheduler

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var express = require('express'),
-	app = express(),
-	http = require('http'),
-	fs = require('fs'),
-	port = process.env.PORT || 3000;
+    app = express(),
+    http = require('http'),
+    fs = require('fs'),
+    port = process.env.PORT || 3000;
 
 app.use('/lib', express.static(__dirname + '/lib'));
-app.get('/sw.bundle.js', function(req, res){
-	res.set('Content-Type', 'application/javascript');
-	res.end(fs.readFileSync('sw.bundle.js'));
+app.get('/sw.bundle.js', (req, res) => {
+    res.set('Content-Type', 'application/javascript');
+    res.end(fs.readFileSync('sw.bundle.js'));
 });
-app.get('/*', function(req, res) {
-	res.end(fs.readFileSync('index.html'));
+app.get('/*', (req, res) => {
+    res.end(fs.readFileSync('index.html'));
 });
 
-http.createServer(app).listen(port, function(err) {
-	console.log('Express server running on port', port);
+http.createServer(app).listen(port, () =>  {
+    console.log('Express server running on port', port);
 });
--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,39 +1,71 @@
 class Scheduler {
 
-	constructor() {
-		this.results = {
-			"js": [],
-			"css": [],
-			"img": [],
-			"fonts": []
-		};
-	}
+    constructor() {
+        this.resources = new Map();
+        this.defaultPrioriy = {
+            'fonts': 1,
+            'css': 2,
+            'js': 3,
+            'images': 4
+        };
+        // Count - Limit when we need to fireaway all requests
+        this.resCount = 0;
+    }
 
-	add(type, req) {
-		let resArr = this.results[type];
-		let priority = this._getResPriority(req);
-		if (resArr !== undefined) {
-			resArr.push(req);
-		}
-	}
+    getResourceType(url) {
+        const requestURL = new URL(url);
+        const arr = requestURL.pathname.split('.');
+        return arr[arr.length - 1];
+    }
 
-	remove(type, req) {
-		let resArr = this.results[type];
-		if (resArr !== undefined) {
-			let index = resArr.indexOf(req);
-			resArr.splice(index, 1);
-		}
-	}
+    fireAllRequests(timeout, callback) {
+        return setTimeout (() => {
+            this.resources.map((type) => {
+                const resArr = this.resources[type];
+                callback(resArr);
+            });
+        }, timeout);
+    }
 
-	_getResPriority(req) {
-		let reqUrl = req.url;
-		let priRegex = /[?&]priority=([^&]+)/i;
-		let value = priRegex.exec(reqUrl);
-		if(value !== null){
-			return value[1];
-		}
-		return 5;
-	}
+    add(request) {
+        this.resCount++;
+        if (this.resCount > 6) {
+            this.resCount = 0;
+            this.fireAllRequests();
+        }
+
+        const type = this.getResourceType(request.url);
+        let resArr = [];
+        if (this.resources.has(type)) {
+            resArr = this.resources.get(type);
+        } else {
+            this.resources.set(type, resArr);
+        }
+        
+        const priority = this._getResPriority(request.url, type);
+        resArr.push(Object.assign(request, {priority: priority}));
+    }
+
+    remove(request) {
+        const type = this.getResourceType(request.url);
+        const resArr = this.resources.get(type);
+        if (resArr !== undefined) {
+            const index = resArr.indexOf(request);
+            resArr.splice(index, 1);
+        }
+    }
+
+    _getResPriority(url, type) {
+        const priRegex = /[?&]priority=([^&]+)/i;
+        const value = priRegex.exec(url);
+        let priority;
+        if (value && value.length > 0) {
+            priority = value[1];
+        } else {
+            priority = this.priority[type];
+        }
+        return priority;
+    }
 }
 
 export default new Scheduler();
