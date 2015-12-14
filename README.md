75940c334b4829c1c5468c455532ba49d995fb89
Vignesh Shanmugam <vignesh.shanmugam22@gmail.com>
2015-12-15 00:30:49 +0100

    Few Changes

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # sw-resource-priority
 Trying out Resource Scheduler with Service Worker. 
 
-Work in progress
+### Priority
+
+These are just assumptions - You can change (Lower the better)
+```
+css : 1
+fonts : 2
+js : 3
+images: 4
+```
+
+### Issue
+
++ Works only after SW install is done - Finding a way to work around
+
 
 ### Development
 
--- src/main.js
+++ src/main.js
@@ -6,27 +6,27 @@ if ('serviceWorker' in navigator &&
     navigator.serviceWorker.register('./sw.bundle.js')
         .then(function (registration) {
             console.log('You are offline');
-            // if (typeof registration.update == 'function') {
-            //     registration.update();
-            // }
-            // registration.onupdatefound = function() {
-            //     let installingWorker = registration.installing;
+            if (typeof registration.update == 'function') {
+                registration.update();
+            }
+            registration.onupdatefound = function() {
+                let installingWorker = registration.installing;
 
-            //     installingWorker.onstatechange = function() {
-            //         switch (installingWorker.state) {
-            //             case 'installed':
-            //                 if (navigator.serviceWorker.controller) {
-            //                     console.log('New or updated content is available.');
-            //                 } else {
-            //                     console.log('Content is cached, and will be available for offline use the ' +
-            //                           'next time the page is loaded.');
-            //                 }
-            //                 break;
-            //             case 'redundant':
-            //                 console.error('The installing service worker became redundant.');
-            //                 break;
-            //         }
-            //     };
+                installingWorker.onstatechange = function() {
+                    switch (installingWorker.state) {
+                        case 'installed':
+                            if (navigator.serviceWorker.controller) {
+                                console.log('New or updated content is available.');
+                            } else {
+                                console.log('Content is cached, and will be available for offline use the ' +
+                                      'next time the page is loaded.');
+                            }
+                            break;
+                        case 'redundant':
+                            console.error('The installing service worker became redundant.');
+                            break;
+                    }
+                };
         })
         .catch(function (e) {
             console.error('Error during service worker registration:', e);
